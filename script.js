297945523db9095f5ae30706ce1a9dea8d80fd34
Sylvie-B <unsolo@hotmail.fr>
2021-01-12 12:38:36 +0100

    const

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const holes = $('.hole');
+//utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const scoreBoard = $('.score');
+// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const moles = $('.mole');
+
